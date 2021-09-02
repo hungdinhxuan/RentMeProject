faa7c5011b4b755975c351eb8af45520cc0f6da9
Khang <khangvippro357@gmail.com>
2021-09-02 13:04:23 +0700

    Header 1/2

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
-import "./Header.scss";
 import { NavLink } from "react-router-dom";
+import "./Header.scss";
 
 function Header() {
+  const [header, setHeader] = useState(false);
+
+  // if (window.pageYOffset) {
+  //   setHeader(true);
+  // }
+
   return (
     <header>
       <Navbar expand="lg">
@@ -16,17 +22,40 @@ function Header() {
               height="50"
               className="d-inline-block align-top"
             />{" "}
-            <span style={{color: "orange"}}>Rent Me</span>
+            <span style={{ color: "orange" }}>Rent Me</span>
           </NavLink>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">StreamHub</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
+              <Nav.Link href="/streamhub">
+                <NavLink to="/streamhub" className="nav__item">
+                  StreamHub
+                </NavLink>
+              </Nav.Link>
+              <Nav.Link>
+                <NavLink to="/news" className="nav__item">
+                  News
+                </NavLink>
+              </Nav.Link>
+              <Nav.Link>
+                <NavLink to="/playerdou" className="nav__item">
+                  Thuê người chơi
+                </NavLink>
+              </Nav.Link>
+              <Nav.Link>
+                <NavLink to="/bxh" className="nav__item">
+                  BXH
+                </NavLink>
+              </Nav.Link>
             </Nav>
             <div className="justify-content-end">
-              <button className="btn btn-success button__login">Log in</button>
-              <button style={{marginLeft: "12px"}} className="btn btn-warning button__signup">Sign up</button>
+              <button className="button__login">Log in</button>
+              <button
+                style={{ marginLeft: "12px" }}
+                className="button__signup"
+              >
+                Sign up
+              </button>
             </div>
           </Navbar.Collapse>
         </Container>
