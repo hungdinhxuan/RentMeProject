d2561af02130bb94b769b1b326b3b31fc3225cd9
Khang <khangvippro357@gmail.com>
2021-09-06 20:26:47 +0700

    Page Not Found

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import Header from "components/Header";
 import { lazy, React, Suspense } from "react";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import CarouselHeader from "features/Carousel";
+import "bootstrap/dist/css/bootstrap.min.css";
+import CarouselHeader from "features/Home/Carousel";
+import PageNotFound from "components/PageNotFound";
+
 function App() {
   return (
     <div className="App">
       <Suspense fallback={<div>Loading...</div>}>
         <BrowserRouter>
           {/* <Header /> */}
-          <CarouselHeader/>
+          <Switch>
+            <Route exact path="/" component={CarouselHeader} />
+            <Route path="*" component={PageNotFound} />
+          </Switch>
         </BrowserRouter>
       </Suspense>
     </div>
