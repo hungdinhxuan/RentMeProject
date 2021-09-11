a5d409464a7117c31baf9f7934c0fdc89c3af7e7
Khang <khangvippro357@gmail.com>
2021-09-11 21:19:08 +0700

    Stories

--- src/features/Home/index.jsx
+++ src/features/Home/index.jsx
@@ -1,17 +1,22 @@
-import Header from 'components/Header'
-import React from 'react'
-import CarouselHeader from './Carousel'
-import ContentHome from './ContentHome'
-import './Home.scss'
+import Header from "components/Header";
+import React from "react";
+import CarouselHeader from "./Carousel";
+import ContentHome from "./ContentHome";
+import "./Home.scss";
+import Stories from "./Stories";
 
 function Home() {
-    return (
-        <div className="home__main">
-            <Header/>
-            <CarouselHeader/>
-            <ContentHome/>
-        </div>
-    )
+  return (
+    <div className="home__main">
+      <Header />
+      <div className="home__body">
+        <CarouselHeader />
+        <ContentHome />
+        <Stories />
+        <div className="bg__home"></div>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
