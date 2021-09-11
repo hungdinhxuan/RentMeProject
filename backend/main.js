ac32f34ee3944d442d139b5968241571aa143220
dinhhungGM <dinhhungGM@gmail.com>
2021-09-11 11:01:07 -0400

    backend | ADD servies mongo atlas, S3 | ADD utils s3, socket | ADD backend docker-compose file

--- backend/main.js
+++ backend/main.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const socket = require('./utils/socket');
+require('dotenv').config();
+require('./services/mongo')();
+
 const app = express();
 
 app.get('/api', (req, res) => {
-    return res.send('server is running')
-})
+  return res.send(`<h1>server is running with bucket ${process.env.AWS_S3_BUCKET_NAME}</h1>`);
+});
 
-app.listen(4000)
+app.listen(4000);
+socket(app)
