b272d8dfc83c3a92e8eb6e3ee6454022489fc7ac
dinhhungGM <dinhhungGM@gmail.com>
2021-09-11 12:26:16 -0400

    ADD mongo, mongo-express local for dev | ADD prod .env file

--- backend/main.js
+++ backend/main.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const socket = require('./utils/socket');
-require('dotenv').config();
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
 require('./services/mongo')();
 
+const swaggerUi = require('swagger-ui-express');
 const app = express();
 
+app.use('/api-docs', swaggerUi.serve);
+
 app.get('/api', (req, res) => {
+    console.log(process.env);
   return res.send(`<h1>server is running with bucket ${process.env.AWS_S3_BUCKET_NAME}</h1>`);
 });
 
