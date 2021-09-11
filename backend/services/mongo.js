66fd43c5c342516dbd5a16d03db0dbe5188de85b
dinhhungGM <dinhhungGM@gmail.com>
2021-09-11 12:26:16 -0400

    ADD mongo, mongo-express local for dev | ADD prod .env file

--- backend/services/mongo.js
+++ backend/services/mongo.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose');
 
 module.exports = async () => {
+  if (process.env.NODE_ENV === 'production') {
+    MONGO_URL = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.rrcyu.mongodb.net/${process.env.MONGODB_DATABASE_NAME}?retryWrites=true&w=majority`;
+  }else{
+    MONGO_URL = `mongodb://test:test@localhost:27017/${process.env.MONGODB_DATABASE_NAME}`
+  }
   try {
-    await mongoose.connect(
-      `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.rrcyu.mongodb.net/${process.env.MONGODB_DATABASE_NAME}?retryWrites=true&w=majority`
-    );
+    await mongoose.connect(MONGO_URL);
     console.log('Connect to database successfully');
   } catch (error) {
-      console.log(error);
+    console.log(error);
     console.log('Cannot connect to database');
   }
 };
