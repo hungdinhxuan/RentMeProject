2fb258fc95f621f75577856c95998d6977f6fd9f
dinhhungGM <dinhhungGM@gmail.com>
2021-09-14 03:17:12 -0400

    B | ADD users, roles models

--- backend/services/mongo.js
+++ backend/services/mongo.js
@@ -4,7 +4,8 @@ module.exports = async () => {
   if (process.env.NODE_ENV === 'production') {
     MONGO_URL = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.rrcyu.mongodb.net/${process.env.MONGODB_DATABASE_NAME}?retryWrites=true&w=majority`;
   }else{
-    MONGO_URL = `mongodb://test:test@localhost:27017/${process.env.MONGODB_DATABASE_NAME}`
+    // MONGO_URL = `mongodb://test:test@localhost:27017/${process.env.MONGODB_DATABASE_NAME}`
+    MONGO_URL = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.rrcyu.mongodb.net/${process.env.MONGODB_DATABASE_NAME}?retryWrites=true&w=majority`;
   }
   try {
     await mongoose.connect(MONGO_URL);
