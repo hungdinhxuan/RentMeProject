e8ecee1eeb2943cbc3e10ef0b4687314349d2ab9
Khang <khangvippro357@gmail.com>
2021-09-14 11:50:09 +0700

    Done Home

---
+++ src/constants/Games.js
@@ -0,0 +1,24 @@
+import amongUs from "assets/ListGames/amongUs.png";
+import apex from "assets/ListGames/apex.png";
+import gta5 from "assets/ListGames/gta5.png";
+import genshinImpact from "assets/ListGames/genshinImpact.png";
+import lol from "assets/ListGames/lol.png";
+import animalCrossing from "assets/ListGames/animalCrossing.png";
+import callOfDuty from "assets/ListGames/callOfDuty.png";
+import deadByDaylight from "assets/ListGames/deadByDaylight.png";
+import dota2 from "assets/ListGames/dota2.png";
+
+
+const Games = [
+  { img: amongUs, name: "amongus" },
+  { img: apex, name: "apex" },
+  { img: gta5, name: "gta5" },
+  { img: genshinImpact, name: "genshin-impact" },
+  { img: lol, name: "league-of-legends" },
+  { img: animalCrossing, name: "animalCrossing" },
+  { img: callOfDuty, name: "callOfDuty" },
+  { img: deadByDaylight, name: "deadByDaylight" },
+  { img: dota2, name: "dota2" },
+
+];
+export default Games;
---
+++ src/features/Home/TryRentme/index.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./TryRentme.scss";
+
+function TryRentme() {
+  return (
+    <div className="container__content" id="try__rentme" data-aos="fade-up" data-aos-duration="1000">
+      <div className="try__content">
+        <div className="try__content--title">
+          Trải nghiệm ngay RentMe với 3 bước dễ dàng
+        </div>
+        <div className="try__content--steps row">
+          <div className="col-lg-4 col-sm-12">
+            <div className="content__steps--items items__1">
+              <div className="text">Chọn dịch vụ</div>
+            </div>
+          </div>
+          <div className="col-lg-4 col-sm-12">
+            <div className="content__steps--items items__2">
+              <div className="text">Chọn thuê người chơi</div>
+            </div>
+          </div>
+          <div className="col-lg-4 col-sm-12">
+            <div className="content__steps--items items__3">
+              <div className="text">Xác nhận giao dịch</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TryRentme;
