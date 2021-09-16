b744dd382e4295f0a7bd45380f6142ec0e6e0a20
Khang <khangvippro357@gmail.com>
2021-09-16 19:54:23 +0700

    Verify Captcha

--- backend/middleware/verifyReptcha.js
+++ backend/middleware/verifyReptcha.js
@@ -1,6 +1,6 @@
 const fetch = require("isomorphic-fetch");
 module.exports = (req, res, next) => {
-  const response_key = req.body['g-recaptcha-response'];
+  const response_key = req.body['captcha'];
   
   // Put secret key here, which we get from google console
   const secret_key = process.env.GOOGLE_RECAPTCHA_SECRET_KEY;
