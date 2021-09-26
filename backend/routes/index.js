2451c288bca95be3ac14ffb90fce6629178aa02c
dinhhungGM <dinhhungGM@gmail.com>
2021-09-26 10:21:22 -0400

    B | ADD cloudinary uplaod, config, dev routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,11 @@
 const authRouter = require('./auth')
 const usersRouter = require('./users')
 const servicesRouter = require('./services')
+const devRouter = require('./dev')
 
 module.exports = (app) => {
     app.use('/api/auth', authRouter)
     app.use('/api/users', usersRouter)
     app.use('/api/services', servicesRouter)
+    app.use('/api/dev', devRouter)
 }
