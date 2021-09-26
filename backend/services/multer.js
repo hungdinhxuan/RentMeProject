7c754363676b3dfd3f2987bf966066d13d783d1d
dinhhungGM <dinhhungGM@gmail.com>
2021-09-26 03:57:10 -0400

    BF | Remove unnessary import and add multer for cloudinary

---
+++ backend/services/multer.js
@@ -0,0 +1,51 @@
+const S3 = require('aws-sdk/clients/s3');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+// require("dotenv").config();
+
+const bucket_name = process.env.AWS_S3_BUCKET_NAME;
+const region = process.env.AWS_S3_BUCKET_REGION;
+const accessKeyId = process.env.AWS_ACCESS_KEY;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+
+const s3 = new S3({
+  accessKeyId,
+  secretAccessKey,
+  region,
+});
+
+const uploadS3 = multer({
+  storage: multerS3({
+    s3,
+    bucket: bucket_name,
+    // acl: "private",
+    metadata: function (req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function (req, file, cb) {
+      cb(null, `${Date.now()}-${file.originalname}`);
+    },
+  }),
+//   fileFilter: (req, file, cb) => {
+//     if (
+//       file.mimetype == 'image/png' ||
+//       file.mimetype == 'image/jpg' ||
+//       file.mimetype == 'image/jpeg'
+//     ) {
+//       cb(null, true);
+//     } else {
+//       cb(null, false);
+//       return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
+//     }
+//   },
+  /// limit 10MB to upload
+  limits: { fileSize: 1024 * 1024 * 10 },
+});
+
+const uploadCloudinary = multer.diskStorage({
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now());
+  }
+});
+
+module.exports = { uploadS3, s3, bucket_name, uploadCloudinary};
