1b0216736913d2b5fa9d95a1d14c156111abb2bd
dinhhungGM <dinhhungGM@gmail.com>
2021-10-03 09:49:07 +0700

    B | ADD dev tools, test case for  auth

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const validate = require('../middleware/validate');
 const verifyRecaptcha = require('../middleware/verifyReptcha');
-const authController = require('../controllers/auth');
+const authController = require('../controllers/auth.controllers');
 const passport = require('passport');
 const {loginLimiter, registerLimiter} = require('../middleware/limitRequests')
 
@@ -10,14 +10,25 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) =>
   res.send(req.user),
 );
 
-router.post(
-  '/login',
-  verifyRecaptcha,
-  validate.validateLogin(),
-  validate.handleValidationErrors,
-  loginLimiter,
-  authController.login,
-);
+
+if(process.env.NODE_ENV === 'production') {
+
+  router.post(
+    '/login',
+    verifyRecaptcha,
+    validate.validateLogin(),
+    validate.handleValidationErrors,
+    loginLimiter,
+    authController.login,
+  );
+}else{
+  router.post(
+    '/login',
+    validate.validateLogin(),
+    validate.handleValidationErrors,
+    authController.login,
+  );
+}
 
 router.post('/google', authController.googleLogin);
 router.post('/facebook', authController.facebookLogin);
---
+++ backend/test/auth.test.js
@@ -0,0 +1,117 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './test.env' });
+const User = require('../models/users');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../main');
+const argon2 = require('argon2');
+chai.should();
+
+chai.use(chaiHttp);
+
+describe('Auth', () => {
+  beforeEach((done) => {
+    //Before each test we empty the database
+    User.deleteMany({}, (err) => {
+      done();
+    });
+  });
+
+  describe('POST auth login', () => {
+    it('it should login success', (done) => {
+      chai
+        .request(server)
+        .post('/api/auth/register')
+        .send({
+          username: 'Admin1',
+          password: 'Abcdef1@',
+          email: 'Admin1@gmail.com',
+          fullName: 'Đinh Hùng Test',
+        })
+        .end((error, result) => {
+          chai
+            .request(server)
+            .post('/api/auth/login')
+            .send({
+              username: 'Admin1',
+              password: 'Abcdef1@',
+            })
+            .end((err, res) => {
+              res.should.have.status(200);
+              // res.body.should.have.property('success').eq(false);
+              // res.body.should.have.property('message').eq('Recaptcha error');
+              res.body.should.have.property('success').eq(true);
+              res.body.should.have.property('message').eq('Login succesful');
+              done();
+            });
+        });
+    });
+  });
+
+  describe('POST auth register', () => {
+    it('it should register successfully', (done) => {
+      chai
+        .request(server)
+        .post('/api/auth/register')
+        .send({
+          username: 'Admintest',
+          password: 'Abcdef1@',
+          email: 'rentmeTest1@gmail.com',
+          fullName: 'Đinh Hùng Test',
+        })
+        .end((err, res) => {
+          res.should.have.status(201);
+          res.body.should.have.property('success').eq(true);
+          res.body.should.have.property('message').eq('created successful !!');
+          res.body.user.should.have.property('username');
+          res.body.user.should.have.property('email');
+          res.body.user.should.have.property('password');
+          res.body.user.should.have.property('fullName');
+          done();
+        });
+    });
+  });
+
+  describe('POST auth register with no field', () => {
+    it('it should error 422 because no valid field is provided', (done) => {
+      chai
+        .request(server)
+        .post('/api/auth/register')
+        .end((err, res) => {
+          res.should.have.status(422);
+          res.body.errors.should.be.a('array');
+          done();
+        });
+    });
+  });
+
+  describe('POST auth register with multiple request', () => {
+    it('it should error 429 because there are too many requests ', (done) => {
+      // .end((err, res) => {
+      //   res.should.have.status(429);
+      //   //   res.body.errors.should.be.a('array')
+      //   done();
+      // });
+      const requester = chai.request(server).keepOpen();
+      let req = [];
+      for (let i = 0; i < 11; i++) {
+        req.push(
+          requester.post('/api/auth/register').send({
+            username: 'Admintest',
+            password: 'Abcdef1@',
+            email: 'rentmeTest1@gmail.com',
+            fullName: 'Đinh Hùng Test',
+          }),
+        );
+      }
+      Promise.all(req)
+        .then((response) => {
+          response[response.length - 1].error.should.have.status(429);
+          done();
+        })
+        .then(() => {
+          requester.close();
+        });
+    });
+  });
+});
