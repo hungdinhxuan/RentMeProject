c13fe1b65d02f24f9c61f8f7033bfbc96ff9f12c
Khang <khangvippro357@gmail.com>
2021-10-05 11:13:14 +0700

    F Protected Route + Authentication Token of User

--- src/components/Applayout/index.jsx
+++ src/components/Applayout/index.jsx
@@ -3,11 +3,6 @@ import Header from "components/Header";
 import React, { Fragment } from "react";
 
 export default function Applayout({ children, ...rest }) {
-  return (
-    <Fragment>
-      <Header />
-      {children}
-      <Footer />
-    </Fragment>
-  );
+  
+  return <Fragment>{children}</Fragment>;
 }
