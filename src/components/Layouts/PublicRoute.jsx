e9fba3e45b04ed869bd032417eef5f1b66f72d5a
dinhhungGM <dinhhungGM@gmail.com>
2021-10-14 22:54:19 +0700

    B | fix socket 2

--- src/components/Layouts/PublicRoute.jsx
+++ src/components/Layouts/PublicRoute.jsx
@@ -2,17 +2,11 @@ import { AsyncLoadUser } from "features/Auth/AuthSlice";
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route } from "react-router";
-<<<<<<< HEAD
 
 
 export function PublicRoute({ component: Component, layout: Layout, ...rest }) {
   const dispatch = useDispatch();
 
-=======
-
-export function PublicRoute({ component: Component, layout: Layout, ...rest }) {
-  const dispatch = useDispatch();
->>>>>>> a2402c92580dd5f19a1f7895fde93057addb14c8
   useEffect(() => {
     if (localStorage.getItem("token")) {
       dispatch(AsyncLoadUser());
--- src/socket.js
+++ src/socket.js
@@ -4,20 +4,8 @@ const socket = io(process.env.REACT_APP_SOCKET_SERVER_URL, {
   path: "/mysocket",
 });
 
-<<<<<<< HEAD
 socket.on("connect", () => {
   socket.emit("authenticate", localStorage.getItem("token"));
 });
 
 export default socket;
-=======
-// Quan trọng: Chứng thực authentication và reconnect lại socket
-export const socketAuth = () => {
-  socket.io.opts.extraHeaders = {
-    Authorization: `Bearer ${localStorage.getItem("token")}`,
-  };
-  socket.io.connect();
-  console.log(`called socket connect with ${localStorage.getItem("token")}`);
-};
-export const socketContext = createContext();
->>>>>>> a2402c92580dd5f19a1f7895fde93057addb14c8
