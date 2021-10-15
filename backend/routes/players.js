ecf7b96576584c266735cfb1d873e92277a6ff7f
dinhhungGM <dinhhungGM@gmail.com>
2021-10-15 23:18:28 +0700

    B | Add api docs

--- backend/routes/players.js
+++ backend/routes/players.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const UserController = require('../controllers/users.controllers');
+const PlayerController = require('../controllers/players.controllers');
 const passport = require('passport');
 
-router.get('/', UserController.filterPlayers)
-router.get('/:id', UserController.getOnePlayer)
+router.get('/', 
+  /*  
+        #swagger.tags = ['Players']  
+       
+    */
+PlayerController.filterPlayers)
+router.get('/:id', 
+  /*  
+        #swagger.tags = ['Players']  
+        #swagger.parameters['id'] = { description: 'Player ID' }
+
+    */
+PlayerController.getOnePlayer)
 
 module.exports = router;
---
+++ backend/swagger.js
@@ -0,0 +1,52 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'RentMe API',
+    description: '',
+  },
+  host: 'localhost:4000',
+  schemes: ['http', 'https'],
+  consumes: ['application/json'],
+  produces: ['application/json'],
+  tags: [
+    {
+        "name": "Auth",
+        "description": "Auth endpoints"
+    },
+    {
+        "name": "Users",
+        "description": "Users endpoints"
+    },
+    {
+      "name": "Services",
+      "description": "Services endpoints"
+    },
+    {
+        "name": "Players",
+        "description": "Players endpoints"
+    },
+    {
+        "name": "Dev",
+        "description": "Dev endpoints"
+    },
+],
+  securityDefinitions: {
+    Authorization: {
+      type: 'apiKey',
+      name: 'Authorization',
+      description: 'Value: Bearer ',
+      in: 'header',
+      scheme: 'bearer',
+    },
+  },
+};
+
+const outputFile = './swagger-output.json';
+const endpointsFiles = ['routes/index.js'];
+
+/* NOTE: if you use the express Router, you must pass in the 
+   'endpointsFiles' only the root file where the route starts,
+   such as index.js, app.js, routes.js, ... */
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
