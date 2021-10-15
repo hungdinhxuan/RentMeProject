01374d0e6b552bafadea4807dc750d3f47671379
Khang <khangvippro357@gmail.com>
2021-10-15 08:14:31 +0700

    F UI Withdraw money

--- src/features/Settings/WalletSetting/index.jsx
+++ src/features/Settings/WalletSetting/index.jsx
@@ -1,30 +1,17 @@
-import React from "react";
-import "./WalletSetting.scss";
-import {
-  Tabs,
-  Form,
-  Input,
-  InputNumber,
-  Cascader,
-  Select,
-  Row,
-  Col,
-  Checkbox,
-  Button,
-  AutoComplete,
-  Radio,
-  Space,
-  Avatar
-} from "antd";
-
+import { Avatar, Button, Form, Input, Radio, Space, Tabs } from "antd";
+import IconMoney from "assets/IconMoney.png";
 import MasterCard from "assets/MasterCard.svg";
-import Zalopay from "assets/zalopay.png";
 import Momo from "assets/Momo.png";
 import Money from "assets/Money.png";
-import IconMoney from "assets/IconMoney.png";
+import Zalopay from "assets/zalopay.png";
+import React from "react";
+import "./WalletSetting.scss";
+import Withdraw from "assets/Withdraw.png";
+import { useEffect } from "react";
 
 const { TabPane } = Tabs;
 export default function WalletSetting() {
+  const [form] = Form.useForm();
   const formItemLayout = {
     labelCol: {
       xs: {
@@ -59,7 +46,15 @@ export default function WalletSetting() {
 
   const handleSubmit = (values) => {
     console.log(values);
+    form.resetFields();
   };
+
+  useEffect(() => {
+    return () => {
+      form.resetFields();
+    };
+  });
+
   return (
     <div className="wallet__setting">
       <div className="wallet__setting--payment">
@@ -88,7 +83,7 @@ export default function WalletSetting() {
                     </div>
 
                     <div className="items-right">
-                      <Form {...formItemLayout} onFinish={handleSubmit}>
+                      <Form {...formItemLayout} form={form}  onFinish={handleSubmit}>
                         <Form.Item
                           name="method"
                           rules={[
@@ -135,8 +130,75 @@ export default function WalletSetting() {
                 </div>
               </TabPane>
               <TabPane tab={<span>Withdraw</span>} key="2">
-                Tab 2
+                <div className="payment-method">
+                  <div className="method-items">
+                    <div className="items-left">
+                      <h2>Withdraw</h2>
+                      <div className="balance">
+                        <img src={Withdraw} alt="Money" />
+                        <div className="balance-content">
+                          <div className="title">Current Money</div>
+                          <Space size={4}>
+                            <div className="icon">
+                              <Avatar src={IconMoney} size={20} />
+                            </div>
+                            <div className="money">
+                              <span>0.00</span>
+                            </div>
+                          </Space>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="items-right">
+                      <Form {...formItemLayout} form={form} onFinish={handleSubmit}>
+                        <Form.Item
+                          name="method"
+                          rules={[
+                            {
+                              required: true,
+                              message: "Please select an option!",
+                            },
+                          ]}
+                        >
+                          <Radio.Group>
+                            <Radio value="mastercard">
+                              <img src={MasterCard} alt="Hinh" />
+                            </Radio>
+                            <Radio value="zalopay">
+                              <img src={Zalopay} alt="Hinh" />
+                            </Radio>
+                            <Radio value="momo">
+                              <img src={Momo} alt="Hinh" />
+                            </Radio>
+                          </Radio.Group>
+                        </Form.Item>
+
+                        <div className="input-money mt-2">
+                          <Form.Item
+                            name="money"
+                            label="Withdraw"
+                            rules={[
+                              {
+                                required: true,
+                                message: "Please input money",
+                              },
+                            ]}
+                          >
+                            <Input />
+                          </Form.Item>
+                        </div>
+
+                        <Form.Item {...tailFormItemLayout}>
+                          <Button htmlType="submit">Submit</Button>
+                        </Form.Item>
+                      </Form>
+                    </div>
+                  </div>
+                </div>
               </TabPane>
+
+              {/* History */}
               <TabPane tab={<span>History</span>} key="3">
                 Tab 3
               </TabPane>
