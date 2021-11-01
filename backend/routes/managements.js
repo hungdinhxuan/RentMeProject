2392ebdc285180065c44a7de5c915e73cbfac3c6
dinhhungGM <dinhhungGM@gmail.com>
2021-11-01 20:38:55 +0700

    B | Add CRUD user management, checkvalid middleware

--- backend/routes/managements.js
+++ backend/routes/managements.js
@@ -5,6 +5,12 @@ const passport = require('passport');
 const { AdminRole } = require('../middlewares/checkRole');
 
 router.get(
+  /*  
+        #swagger.tags = ['Managements']
+        #swagger.security = [{
+            "Authorization": []
+        }]
+    */
   '/users',
   passport.authenticate('jwt', { session: false }),
   AdminRole,
@@ -12,6 +18,12 @@ router.get(
 );
 
 router.post(
+  /*  
+        #swagger.tags = ['Managements']
+        #swagger.security = [{
+            "Authorization": []
+        }]
+    */
   '/users',
 
   passport.authenticate('jwt', { session: false }),
@@ -19,4 +31,55 @@ router.post(
   ManagementController.createUser,
 );
 
+router.patch(
+  /*  
+        #swagger.tags = ['Managements']
+        #swagger.security = [{
+            "Authorization": []
+        }]
+    */
+  '/users',
+  passport.authenticate('jwt', { session: false }),
+  AdminRole,
+  ManagementController.restoreUsers,
+);
+
+router.delete(
+  /*  
+        #swagger.tags = ['Managements']
+        #swagger.security = [{
+            "Authorization": []
+        }]
+    */
+  '/users/soft',
+  passport.authenticate('jwt', { session: false }),
+  AdminRole,
+  ManagementController.softDeleteUsers,
+);
+
+router.delete(
+  /*  
+        #swagger.tags = ['Managements']
+        #swagger.security = [{
+            "Authorization": []
+        }]
+    */
+  '/users/force',
+  passport.authenticate('jwt', { session: false }),
+  AdminRole,
+  ManagementController.forceDeleteUsers,
+);
+
+router.put(
+  '/users',
+  /*  
+        #swagger.tags = ['Managements']
+        #swagger.security = [{
+            "Authorization": []
+        }]
+    */
+  passport.authenticate('jwt', { session: false }),
+  AdminRole,
+  ManagementController.updateUser,
+);
 module.exports = router;
