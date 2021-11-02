a1316cdfb5a04cd939d36c84250069e28abfb2b7
dinhhungGM <dinhhungGM@gmail.com>
2021-11-02 11:05:37 +0700

    F | fix merge bug admin

--- src/features/Admin/AdminSlice.js
+++ src/features/Admin/AdminSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import axiosClient from "axiosClient";
+import axiosClient from "utils/axiosClient";
 
 const initialState = {
   userList: [],
