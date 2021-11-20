e98bb07ac2387eedd322e97ba7955807712bb3d9
dinhhungGM <dinhhungGM@gmail.com>
2021-11-20 14:32:16 +0700

    F | fix messager customer chat position

--- src/components/Layouts/PublicLayout.jsx
+++ src/components/Layouts/PublicLayout.jsx
@@ -1,4 +1,13 @@
 import React, { Fragment } from "react";
+import MessengerCustomerChat from "react-messenger-customer-chat"
+
 export function PublicLayout({ children, ...rest }) {
-  return <Fragment>{children}</Fragment>;
+  return <Fragment>
+    {children}
+    <MessengerCustomerChat
+          pageId={process.env.REACT_APP_FACEBOOK_PAGE_ID}
+          appId={process.env.REACT_APP_FACEBOOK_APP_ID}
+          htmlRef={process.env.REACT_APP_FACEBOOK_HTML_REF}
+      />
+    </Fragment>;
 }
